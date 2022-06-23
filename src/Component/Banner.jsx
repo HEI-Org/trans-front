60a750ebe169fe0819e3cd3ddbd033b7fcaff2e7
fanjasoa <fanjasoa18@gmail.com>
2022-06-23 15:40:14 +0300

    feat: add banner component

---
+++ src/Component/Banner.jsx
@@ -0,0 +1,14 @@
+import './css/banner.css';
+import React from "react";
+
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className="container">
+                <a href="" className="btn btn-warning">Reserver</a>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
