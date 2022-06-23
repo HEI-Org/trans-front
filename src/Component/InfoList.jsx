f3df3ac097edb10b08a90f687053b0f176377262
fanjasoa <fanjasoa18@gmail.com>
2022-06-23 15:01:37 +0300

    feat: add info table

---
+++ src/Component/InfoList.jsx
@@ -0,0 +1,15 @@
+import DropDown from "./DropDown";
+
+export default function InfoList() {
+    return (
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>
+                <DropDown/>
+            </td>
+            <td>1000</td>
+        </tr>
+    )
+}
---
+++ src/Component/InfoTable.jsx
@@ -0,0 +1,28 @@
+import InfoList from "./InfoList";
+
+function InfoTable() {
+    return (
+        <div className='container mt-5'>
+            <h3>TARIFS</h3>
+            <table className="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Départ</th>
+                    <th scope="col">Arrivée</th>
+                    <th scope="col">Offre</th>
+                    <th scope="col">Frais</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <InfoList/>
+                    <InfoList/>
+                    <InfoList/>
+                    <InfoList/>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default InfoTable;
