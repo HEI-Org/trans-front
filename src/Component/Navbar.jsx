134eeb2b1959f3fb64fc4aa71bdc493ab0ddd912
fanjasoa <fanjasoa18@gmail.com>
2022-06-23 15:00:18 +0300

    feat: add banner image

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,9 +1,11 @@
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light">
             <a className="navbar-brand" href="#">TokyTrans</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"/>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -14,11 +16,13 @@ function Navbar() {
                     <li className="nav-item">
                         <a className="nav-link" href="#">Offre</a>
                     </li>
-                    
+
                 </ul>
+                <button className='btn btn-warning text-dark font-weight-bold mr-2'>Reserver</button>
                 <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Entrer une ville" aria-label="Search"/>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">rechercher</button>
+                    <input className="form-control mr-sm-2" type="search" placeholder="Entrer une ville"
+                           aria-label="Search"/>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">rechercher</button>
                 </form>
             </div>
         </nav>
